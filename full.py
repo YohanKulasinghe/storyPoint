9f6694605e62bb91a3cccdf1ec069fe649316c6d
yohan kulasinghe <yohankulasinghe@gmail.com>
2020-12-12 21:02:03 +0530

    corected MMRE

--- full.py
+++ full.py
@@ -17,7 +17,7 @@ trainT=sc_y.fit_transform(trainT)
 
 trainT = trainT.ravel()
 from sklearn.svm import SVR
-r=SVR(kernel="rbf")
+r=SVR(kernel='rbf', C = 0.78121, tol = 0.05 )
 r.fit(trainSP,trainT)
 
 plt.scatter(sc.inverse_transform(trainSP),sc_y.inverse_transform(trainT), color="red")
@@ -57,6 +57,60 @@ n = len(testSetActualTime)
 MMRE = 0
 
 for i in range(n):
-    MMRE = MMRE + (abs(testSetActualTime[i]-testSetPredictedTime[i]))/testSetActualTime[i]
+    print(abs(testSetActualTime[i]-testSetPredictedTime[i])/testSetActualTime[i])
+    MMRE = MMRE + (abs(testSetActualTime[i]-testSetPredictedTime[i])/testSetActualTime[i])
 
-print("\nMMRE = %f" % (MMRE))
+MMRE = MMRE/n * 100
+
+print("\nMMRE For time= %f " % (MMRE))
+
+# temp1 = 0
+
+# for i in range(n):
+#     temp1 = temp1 + abs(testSetActualTime[i]-testSetPredictedTime[i])
+
+# print(temp1)
+# PRED = 1 - (temp1 / n) * 100
+# print(PRED)
+
+team_salary=560679
+non_tech_salary=183451
+equipment=34821
+depreciation=8736
+rent=14634
+travelling=38279
+furniture=2356
+utilityBills=27541
+coppyright=15239
+software_purchase=12781
+repair_and_maintenance=8393
+sanitary=5782
+marketing=4782
+other=24790
+
+fraction_non_tech_salary= non_tech_salary / team_salary
+fraction_equipment=equipment / team_salary
+fraction_depreciation=depreciation / team_salary
+fraction_rent=rent / team_salary
+fraction_travelling=travelling / team_salary
+fraction_furniture=furniture / team_salary
+fraction_utilityBills=utilityBills / team_salary
+fraction_coppyright=coppyright / team_salary
+fraction_software_purchase=software_purchase / team_salary
+fraction_repair_and_maintenance=repair_and_maintenance / team_salary
+fraction_sanitary=sanitary / team_salary
+fraction_marketing=marketing / team_salary
+fraction_other= other / team_salary
+
+cost_driver = 1 + fraction_non_tech_salary + fraction_equipment + fraction_depreciation + \
+    fraction_rent + fraction_travelling + fraction_furniture + \
+    fraction_utilityBills + fraction_coppyright + fraction_software_purchase + \
+    fraction_repair_and_maintenance + fraction_sanitary + \
+    fraction_marketing + fraction_other
+
+print(cost_driver)
+
+predicted_cost = cost_driver * 230000 * 58/30
+
+print(predicted_cost)
+
